4ba779d8598e6bb39e09b3a5014fb02714adf554
Zeshan Munir <zmunir93@gmail.com>
2018-01-08 18:47:06 -0500

    modified route root

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 get 'index' => 'url#index'
 
-root '/'
+root to: 'url#new'
 
 post '/' => 'url#create'
 
